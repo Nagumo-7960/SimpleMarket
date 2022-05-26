1ec39dfd5ade3a4c3492a4874c58b8382b1f1f2a
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-05-26 22:11:39 +0900

    bottomの表示

--- SimpleMarket/app/src/main/java/com/example/simplemartket/BottomNavItem.kt
+++ SimpleMarket/app/src/main/java/com/example/simplemartket/BottomNavItem.kt
@@ -1,9 +1,65 @@
 package com.example.simplemartket
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 
 data class BottomNavItem(
     val name:String,
     val route:String,
-    val icon:ImageVector
+    val icon: ImageVector
 )
+
+@Composable
+fun BottomNavigationBar(
+    items:List<BottomNavItem>,
+    navController:NavController,
+    modifier: Modifier,
+    onItemClick:(BottomNavItem) -> Unit
+){
+    val backStackEntry = navController.currentBackStackEntryAsState()
+    BottomNavigation (
+        modifier = modifier,
+        backgroundColor = Color.White,
+        elevation = 5.dp
+    ){
+        items.forEach { item ->
+            val selected = item.route == backStackEntry.value?.destination?.route
+            BottomNavigationItem(
+                selected = selected,
+                onClick = { onItemClick(item) },
+                selectedContentColor = Color.DarkGray,
+                unselectedContentColor = Color.Gray,
+                icon = {
+                    Column(horizontalAlignment = CenterHorizontally) {
+                        Icon(imageVector = item.icon, contentDescription = item.name)
+                        if(selected){
+                            Text(text = item.name,
+                                textAlign = TextAlign.Center,
+                                fontSize = 10.sp
+                            )
+                        }
+                    }
+
+                }
+
+            )
+        }
+    }
+
+}
--- SimpleMarket/app/src/main/java/com/example/simplemartket/MainActivity.kt
+++ SimpleMarket/app/src/main/java/com/example/simplemartket/MainActivity.kt
@@ -5,8 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,7 +23,34 @@ class MainActivity : ComponentActivity() {
         setContent {
             val navController = rememberNavController()
             Navigation(navController = navController)
+
+            Scaffold(
+                bottomBar = {
+                    BottomNavigationBar(
+                        items = listOf(
+                            BottomNavItem(
+                                name = "Home",
+                                route = "home",
+                                icon = Icons.Default.Search
+                            ),
+                            BottomNavItem(
+                                name = "favorite",
+                                route = "favorite",
+                                icon = Icons.Default.Favorite
+                            )
+                        ),
+                        navController = navController,
+                        modifier = Modifier,
+                        onItemClick ={
+                            navController.navigate(it.route)
+                        }
+                    )
+                }
+            ) {
+                Navigation(navController = navController)
+            }
         }
     }
 }
+
 
