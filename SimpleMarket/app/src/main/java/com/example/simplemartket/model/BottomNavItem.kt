a55f6a920c00206aaecdd0697cdc67f9bda1cc71
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-05-28 22:55:33 +0900

    適切な階層にファイルを移動

---
+++ SimpleMarket/app/src/main/java/com/example/simplemartket/model/BottomNavItem.kt
@@ -0,0 +1,28 @@
+package com.example.simplemartket.model
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.simplemartket.ui.theme.ThemeColor
+
+data class BottomNavItem(
+    val name:String,
+    val route:String,
+    val icon: ImageVector
+)
+
--- SimpleMarket/app/src/main/java/com/example/simplemartket/ui/components/BottomBar.kt
+++ SimpleMarket/app/src/main/java/com/example/simplemartket/ui/components/BottomBar.kt
@@ -1,35 +1,23 @@
-package com.example.simplemartket
+package com.example.simplemartket.ui.components
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.simplemartket.model.BottomNavItem
 import com.example.simplemartket.ui.theme.ThemeColor
-
-data class BottomNavItem(
-    val name:String,
-    val route:String,
-    val icon: ImageVector
-)
 
 @Composable
 fun BottomNavigationBar(
     items:List<BottomNavItem>,
-    navController:NavController,
+    navController: NavController,
     modifier: Modifier,
     onItemClick:(BottomNavItem) -> Unit
 ){
@@ -47,7 +35,7 @@ fun BottomNavigationBar(
                 selectedContentColor = ThemeColor,
                 unselectedContentColor = Color.Gray,
                 icon = {
-                    Column(horizontalAlignment = CenterHorizontally) {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
                         Icon(imageVector = item.icon, contentDescription = item.name)
                     }
 
