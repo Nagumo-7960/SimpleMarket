f31c06c959e2bc4bdc9deb8f5e0c0cb9cf3a2dab
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-05-30 20:27:51 +0900

    Productのリストを作成

--- SimpleMarket/app/src/main/java/com/example/simplemartket/model/Product.kt
+++ SimpleMarket/app/src/main/java/com/example/simplemartket/model/Product.kt
@@ -6,3 +6,24 @@ data class Product(
     val imageUrl:String,
     val value:Int
 )
+
+val Products = listOf(
+    Product(
+        id = 1,
+        name = "季節の美味しいぶどう",
+        imageUrl = "https://d.kuku.lu/bef40a330",
+        value = 2000
+    ),
+    Product(
+        id = 2,
+        name = "売れ残ったみかん",
+        imageUrl = "https://d.kuku.lu/e9c62ac40",
+        value = 3000
+    ),
+    Product(
+        id = 3,
+        name = "野山のいちご",
+        imageUrl = "https://d.kuku.lu/c3a650705",
+        value = 2300
+    )
+)
