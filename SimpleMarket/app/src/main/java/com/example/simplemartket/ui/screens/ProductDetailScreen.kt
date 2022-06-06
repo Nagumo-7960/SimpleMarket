b8b0cb1c37dd8f81d8221759da7f276d7d5e58c9
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-06-06 22:15:43 +0900

    詳細画面の画像の大きさを調整

--- SimpleMarket/app/src/main/java/com/example/simplemartket/ui/screens/ProductDetailScreen.kt
+++ SimpleMarket/app/src/main/java/com/example/simplemartket/ui/screens/ProductDetailScreen.kt
@@ -1,24 +1,49 @@
 package com.example.simplemartket.ui.screens
 
-import android.media.Image
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.example.simplemartket.model.Product
 import com.example.simplemartket.model.Products
 
 @Composable
-fun ProductDetailScreen(toDetail:() -> Unit){
-    Column() {
+fun ProductDetailScreen(toDetail: () -> Unit) {
+    Column {
         ProductDetail(products = Products.first())
     }
 }
 
 @Composable
-fun ProductDetail(products: Product){
+fun ProductDetail(products: Product) {
     val painter = rememberImagePainter(products.imageUrl)
-    Image(
-        painter = painter,
-        contentDescription = "product_image")
+    Column(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Image(
+                painter = painter,
+                contentDescription = "product_image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    //画像の横幅を最大にすることで強引にサイズ調整しているので修正予定
+                    .width(5000.dp)
+                    .height(300.dp)
+            )
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun PreviewProductDetail() {
+    ProductDetailScreen {
+
+    }
 }
