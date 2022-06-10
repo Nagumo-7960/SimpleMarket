fa6b88c297cdd27c6cc0b09ae4d885aa14352e48
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-06-10 09:44:52 +0900

    お気に入りボタンを押した時にアイコンの色が変わる

--- SimpleMarket/app/src/main/java/com/example/simplemartket/ui/components/FavoriteButton.kt
+++ SimpleMarket/app/src/main/java/com/example/simplemartket/ui/components/FavoriteButton.kt
@@ -1,17 +1,17 @@
 package com.example.simplemartket.ui.components
 
 import android.util.Log
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,8 +20,10 @@ import com.example.simplemartket.model.Product
 import com.example.simplemartket.model.Products
 import com.example.simplemartket.ui.theme.ThemeColor
 
+
 @Composable
 fun FavoriteButton(product: Product) {
+    var checked = remember { mutableStateOf(false) }
     Button(
         onClick = {
             product.favorite = product.favorite == false
@@ -41,13 +43,23 @@ fun FavoriteButton(product: Product) {
         )
     ) {
         // Inner content including an icon and a text label
-        Icon(
-            Icons.Filled.Star,
-            contentDescription = "Favorite",
-            modifier = Modifier.size(ButtonDefaults.IconSize)
-        )
+        IconToggleButton(checked = checked.value,
+            onCheckedChange = { checked.value = it })
+        {
+            val tint = animateColorAsState(
+                if (checked.value) Color(0xFFEC407A) else Color(0xFFB0BEC5)
+            )
+            Icon(
+                Icons.Filled.Star,
+                contentDescription = "Favorite",
+                tint = tint.value,
+                modifier = Modifier.size(50.dp)
+            )
+
+        }
         Spacer(Modifier.size(ButtonDefaults.IconSpacing))
         Text("お気に入り")
+
     }
 }
 
