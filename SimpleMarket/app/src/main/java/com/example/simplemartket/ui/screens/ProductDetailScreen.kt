f24379e6973faf37dfe09d1d39b39da1e7db10de
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-06-22 16:46:48 +0900

    ProductDetailをProductDetailScreenにまとめる

--- SimpleMarket/app/src/main/java/com/example/simplemartket/ui/screens/ProductDetailScreen.kt
+++ SimpleMarket/app/src/main/java/com/example/simplemartket/ui/screens/ProductDetailScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -21,14 +22,85 @@ import com.example.simplemartket.ui.components.NonFavoriteButton
 
 @Composable
 fun ProductDetailScreen(toDetail: Int, viewModel: FavoriteScreenViewModel) {
-    //val favoriteState by viewModel.favoriteState.observeAsState(initial = false)
+    val favoriteState = viewModel.favoriteState.observeAsState()
     Log.d("favorite", "ProductDetailScreenが呼ばれる")
     LazyColumn(
         modifier = Modifier
             .fillMaxWidth()
     ) {
         item {
-            ProductDetail(products = Products.get(toDetail), viewModel = viewModel)
+            val painter = rememberImagePainter(Products.get(toDetail).imageUrl)
+            Column(modifier = Modifier.fillMaxSize()) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Image(
+                        painter = painter,
+                        contentDescription = "product_image",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            //画像の横幅を最大にすることで強引にサイズ調整しているので修正予定
+                            .width(5000.dp)
+                            .height(300.dp)
+                    )
+                }
+                Column(
+                    modifier = Modifier
+                        .padding(
+                            start = 20.dp
+                        )
+                ) {
+                    Text(
+                        text = Products.get(toDetail).name,
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = "料金",
+                        fontSize = 20.sp,
+                        modifier = Modifier
+                            .padding(
+                                top = 20.dp
+                            )
+                    )
+                    Text(
+                        text = Products.get(toDetail).value.toString() + "円",
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = Products.get(toDetail).explanation,
+                        fontSize = 20.sp,
+                        modifier = Modifier
+                            .padding(
+                                top = 20.dp
+                            )
+                    )
+                }
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .padding(
+                            top = 20.dp
+                        )
+                        .fillMaxWidth()
+                ) {
+                    if (viewModel.favoriteState.value == true) FavoriteButton(
+                        Products.get(toDetail)){
+                        viewModel.changeFavoriteState()
+                    }
+                    else NonFavoriteButton(Products.get(toDetail)){
+                        viewModel.changeFavoriteState()
+                    }
+                }
+                Column (
+                    modifier = Modifier
+                        .height(150.dp)
+                ){
+
+                }
+            }
         }
     }
 }
@@ -37,78 +109,6 @@ fun ProductDetailScreen(toDetail: Int, viewModel: FavoriteScreenViewModel) {
 fun ProductDetail(products: Product, viewModel: FavoriteScreenViewModel) {
 
 //    Log.d("favorite", viewModel.favoriteState.toString())
-    val painter = rememberImagePainter(products.imageUrl)
-    Column(modifier = Modifier.fillMaxSize()) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-        ) {
-            Image(
-                painter = painter,
-                contentDescription = "product_image",
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    //画像の横幅を最大にすることで強引にサイズ調整しているので修正予定
-                    .width(5000.dp)
-                    .height(300.dp)
-            )
-        }
-        Column(
-            modifier = Modifier
-                .padding(
-                    start = 20.dp
-                )
-        ) {
-            Text(
-                text = products.name,
-                fontSize = 30.sp,
-                fontWeight = FontWeight.Bold
-            )
-            Text(
-                text = "料金",
-                fontSize = 20.sp,
-                modifier = Modifier
-                    .padding(
-                        top = 20.dp
-                    )
-            )
-            Text(
-                text = products.value.toString() + "円",
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold
-            )
-            Text(
-                text = products.explanation,
-                fontSize = 20.sp,
-                modifier = Modifier
-                    .padding(
-                        top = 20.dp
-                    )
-            )
-        }
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier
-                .padding(
-                    top = 20.dp
-                )
-                .fillMaxWidth()
-        ) {
-            if (viewModel.favoriteState.value == true) FavoriteButton(
-                products){
-                viewModel.changeFavoriteState()
-            }
-            else NonFavoriteButton(products){
-                viewModel.changeFavoriteState()
-            }
-        }
-        Column (
-            modifier = Modifier
-                .height(150.dp)
-                ){
-
-        }
-    }
 
 
 
